f28685da1eb9c42ea86b11c1443005fabd6dd7fa
cg <654450188@qq.com>
2017-06-13 18:42:45 +0800

    进度条 音量条 歌词滚动

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,19 @@ var $next = $(".control .next")
 var $pre = $(".control .pre")
 var $disc = $(".disc")
 
+var timeNode = $('.musicbox .time')
+var progressBarNode = $('.timeline .progress .bar')
+var progressNowNode = $('.timeline .progress-now')
 
+var voiceBarNode = $('.adjust-volume .progress .bar')
+var voiceNowNode = $('.adjust-volume .progress-now')
+
+var $lrc = $(".lrc .icon-lrc")
+var $dis = $(".lrc .icon-dis")
+
+var $picCt = $("main .pic-ct")
+var $lrcCt = $("main .lrc-ct")
+// console.log($lrcCt);
 
 var music = {
   channelId: "", //专辑的ID
@@ -28,10 +40,13 @@ var music = {
   url: "", //歌曲的地址
 }
 
-$(getMusicRandom)
+//页面加载完成即获取随机音乐
+$(function(){
+  getMusicRandom()
+})
 
 //点击获取专辑按钮
-$album.on("click", function(){
+$album.on("click", function() {
   getMusicAlbum()
   $albumList.toggleClass("hidden")
 })
@@ -41,28 +56,31 @@ $album.on("click", "li", function(e) {
   e.stopPropagation();
   var id = $(this).data("channel-id")
   music.channelId = id;
-  console.log(id);
+  // console.log(id);
   getMusicRandom(music.channelId) //根据 id 获取随机音乐
   $pause.css("display", "none")
   $play.css("display", "flex")
   $disc.addClass("active")
-  $albumList.css('display', 'none')
+  $albumList.addClass("hidden")
 })
 
-$next.on("click", function(){
+//点击获取下一首，本专辑内
+$next.on("click", function() {
   getMusicRandom(music.channelId)
   $pause.css("display", "none")
   $play.css("display", "flex")
   $disc.addClass("active")
 })
 
-$pre.on("click", function(){
+//点击获取上一首，本专辑内
+$pre.on("click", function() {
   getMusicRandom(music.channelId)
   $pause.css("display", "none")
   $play.css("display", "flex")
   $disc.addClass("active")
 })
 
+//点击播放按钮，变成暂停状态
 $play.on("click", function() {
   audioObject.pause()
   $play.css("display", "none")
@@ -70,11 +88,51 @@ $play.on("click", function() {
   $disc.removeClass("active")
 })
 
+//点击暂停按钮，变成播放状态
 $pause.on("click", function() {
   audioObject.play()
   $pause.css("display", "none")
   $play.css("display", "flex")
   $disc.addClass("active")
+})
+
+//点击进度条，调整播放时间
+progressBarNode.on("click", function(e) {
+  // console.log($(this).width());
+  var percent = e.offsetX / parseInt($(this).width())
+  audioObject.currentTime = percent * audioObject.duration
+  progressNowNode.width(percent * 100 + "%")
+})
+
+//播放时候触发函数
+audioObject.ontimeupdate = function() {
+  setProgress() //设置进度条
+  setTimeNode() //设置播放时间
+  setVoice() //设置当前音量
+  slideLrc() //滑动歌词
+}
+
+//设置音量
+voiceBarNode.on("click", function(e) {
+  var percent = e.offsetX / parseInt($(this).width())
+  audioObject.volume = percent
+  voiceNowNode.width(percent * 100 + "%")
+})
+
+//点击显示歌词
+$lrc.on("click", function() {
+  $lrc.css('display', 'none')
+  $dis.css('display', 'inline-block')
+  $picCt.css('display', 'none')
+  $lrcCt.css('display', 'flex')
+})
+
+//点击显示唱片
+$dis.on("click", function() {
+  $lrc.css('display', 'inline-block')
+  $dis.css('display', 'none')
+  $picCt.css('display', 'flex')
+  $lrcCt.css('display', 'none')
 })
 
 //获取音乐专辑
@@ -104,8 +162,9 @@ function appendMusicAlbum(data) {
 
 //获取随机音乐
 function getMusicRandom(id) {
+  var id = id || 'public_yuzhong_huayu' //如果没有专辑id,默认为华语专辑
   var musicUrl = "https://jirenguapi.applinzi.com/fm/getSong.php?channel=" + id
-  //console.log(url);
+  console.log(musicUrl);
   $.ajax({
     type: 'get',
     url: musicUrl,
@@ -155,14 +214,120 @@ function getLrc() {
     url: lrcUrl,
     dataType: "json",
   }).then((data) => {
-    console.log(data);
-    appendLic(data);
+    // console.log(data);
+    arrLic(data); //把歌词做成一个二维数组
   }, (error) => {
     alert('歌词获取失败')
   })
 }
 
-//渲染歌词
-function appendLic(data) {
-  $("main>pre").html(data.lyric)
+//把歌词做成一个二维数组
+function arrLic(data) {
+  var lyricArr = [];
+  var lyric = data.lyric
+  // console.log(typeof lyric);
+  var lines = lyric.split("\n")
+  // console.log(lines);
+  var timeRegular = /\[\d{2}:\d{2}\.\d{2}\]/g
+  lines.forEach((item) => {
+    if (!timeRegular.test(item)) {
+      lines.splice(item, 1)
+      return;
+    }
+    var time = item.match(timeRegular)
+    // console.log(time[0]);
+    // console.log(typeof time[0]);
+    // console.log(time[0][1]);
+    var lyric = item.split(time)
+    // console.log(lyric);
+    var seconds = time[0][1] * 600 + time[0][2] * 60 + time[0][4] * 10 + time[0][5] * 1
+    // console.log(seconds);
+    lyricArr.push([seconds, lyric[1]])
+    // console.log(lyricArr);
+  })
+  appendLic(lyricArr) //渲染歌词
+}
+
+function appendLic(lyricArr) {
+  var lyricLength = 0;
+  var html = `<div class="lyric-ani" data-height="20">`;
+  lyricArr.forEach((element, index) => {
+    var ele = element[1] === undefined ? '歌词错误' : element[1];
+    html += `<p class="lyric-line" data-id="${index}"  data-time="${element[0]}" > ${ele} </p> \n`
+    lyricLength++;
+  })
+  html += `</div>`
+  // console.log(html);
+  // console.log(lyricLength);
+  $lrcCt.html(html)
+  // onTimeUpdate(lyricLength);
+}
+
+// function onTimeUpdate(lyricLength){
+//   // console.log($lrcCt);
+//   var lyricAni = $(".lyric-ani")
+//   // console.log(lyricAni);
+//   var lyricH = lyricAni.data("height")
+//   // console.log($lrcCt);
+//   var lyricP = $lrcCt.find('.lyric-ani p');
+//   // console.log(lyricP);
+//   var curTime = audioObject.currentTime;
+//   var content = audioObject.duration;
+// }
+
+
+
+//设置进度条
+function setProgress() {
+  var timeGo = (audioObject.currentTime / audioObject.duration) * progressBarNode.width()
+  progressNowNode.width(timeGo)
+}
+
+//设置剩余时间显示
+function setTimeNode() {
+  var minute = parseInt(audioObject.currentTime / 60)
+  var second = parseInt(audioObject.currentTime % 60)
+  if (second < 10) {
+    second = "0" + second
+  }
+  // console.log(second);
+  timeNode.text(minute + ":" + second)
+}
+
+//设置音量条的长度
+function setVoice() {
+  var voice = audioObject.volume
+  // console.log(voice);
+  voiceNowNode.width(voice * 100 + "%")
+}
+
+//设置歌词滚动
+function slideLrc() {
+  //............根据时间滚动歌词
+  // console.log($lrcCt);
+  var lyricAni = $(".lyric-ani")
+  // console.log(lyricAni);
+  var lyricH = lyricAni.data("height")
+  // console.log($lrcCt);
+  var lyricP = $lrcCt.find('.lyric-ani p');
+  // console.log(lyricP);
+  var curTime = audioObject.currentTime;
+  var content = audioObject.duration;
+  // console.log(lyricP.length);
+  for (var i = 0; i < lyricP.length; i++) {
+    var curT = lyricP.eq(i).data("time")
+    var nexT = lyricP.eq(i + 1).data("time")
+    if ((curTime > curT) && (curTime < nexT)) {
+      lyricP.eq(i).addClass("active");
+    }
+    if (curTime > nexT) {
+      lyricP.removeClass('active');
+      lyricAni.css({
+        'height': lyricH * lyricP.length + 'px',
+        'marginTop': -parseInt(lyricH * i - 100) + 'px',
+        "transition": "2s"
+      });
+    }
+  }
+
 }
